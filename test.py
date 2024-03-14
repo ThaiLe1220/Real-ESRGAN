ba4e57b381814719e109c29697375ff39343f7a7
ThaiLe1220 <lehongthai2000@gmail.com>
2024-03-14 12:12:26 +0700

    update test folder

--- test.py
+++ test.py
@@ -3,19 +3,29 @@ import time
 import psutil
 import GPUtil
 
-# The command you want to run
-command = "./realesrgan-ncnn-vulkan -i ./images/768x768.png -o 768x768.png "
+# List of commands you want to run
+commands = [
+    "./realesrgan-ncnn-vulkan -i ./images/768x768.png -o 768x768-animevideo.png -n realesr-animevideov3",
+    "./realesrgan-ncnn-vulkan -i ./images/768x768.png -o 768x768-x4plus.png -n realesrgan-x4plus",
+    "./realesrgan-ncnn-vulkan -i ./images/768x768.png -o 768x768-x4plusanime.png -n realesrgan-x4plus-anime",
+    "./realesrgan-ncnn-vulkan -i ./images/600x600.png -o 600x600-animevideo.png -n realesr-animevideov3",
+    "./realesrgan-ncnn-vulkan -i ./images/600x600.png -o 600x600-x4plus.png -n realesrgan-x4plus",
+    "./realesrgan-ncnn-vulkan -i ./images/600x600.png -o 600x600-x4plusanime.png -n realesrgan-x4plus-anime",
+]
 
-# Start the timer
-start_time = time.time()
+# Iterate over the list of commands and execute them one by one
+for command in commands:
+    # Start the timer
+    start_time = time.time()
 
-# Run the command
-process = subprocess.run(command, shell=True)
+    # Run the command
+    process = subprocess.run(command, shell=True, check=True)
 
-# Calculate the elapsed time
-elapsed_time = time.time() - start_time
+    # Calculate the elapsed time
+    elapsed_time = time.time() - start_time
 
-if process.returncode == 0:
-    print(f"The command executed successfully and took {elapsed_time:.2f} seconds.")
-else:
-    print(f"The command failed with return code {process.returncode}.")
+    # Check the exit code and print the appropriate message
+    if process.returncode == 0:
+        print(f"The command executed successfully and took {elapsed_time:.2f} seconds.")
+    else:
+        print(f"The command failed with return code {process.returncode}.")
